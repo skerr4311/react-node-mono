59c35857b7856a97d78f2ba4c53d53860ed1aba8
Steve Kerr <sker078@aucklanduni.ac.nz>
2025-02-01 19:48:44 +1300

    complete selected patient

--- apps/react-frontend/src/pages/Home/home.tsx
+++ apps/react-frontend/src/pages/Home/home.tsx
@@ -1,10 +1,20 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
+import { Patient } from '@mono-repo/api-clients/patient-api';
 import { Table } from '@mono-repo/ui-shared';
 import { usePatients } from '@mono-repo/api';
 
 export const HomePage: FC = () => {
+  const [selectedPatient, setSelectedPatient] = useState<Patient>();
   const { data: patients } = usePatients();
 
-  return <Table patients={patients} onRowClick={(patient) => console.log(patient)} />;
+  const handleSelectedPatient = (patient: Patient | undefined) => setSelectedPatient(patient);
+
+  return (
+    <Table
+      patients={patients}
+      onRowClick={(patient) => handleSelectedPatient(patient)}
+      selectedPatient={selectedPatient}
+    />
+  );
 };
